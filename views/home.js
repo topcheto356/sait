979efd3ef339df6abf0ca824bd2026c31d3a5536
Kamenow <iavorkamenov777@gmail.com>
2023-11-07 22:55:45 +0200

    home page

---
+++ views/home.js
@@ -0,0 +1,33 @@
+const homeView = `<div class="home-container">
+  <div class="home-banner">
+    <img
+      class="home-banner-image"
+      src="./assets/js/images/homeView/banner.png"
+      alt=""
+    />
+  </div>
+  <div class="home-description">
+    <p>
+      Спортен клуб по ориентиране “Туида” Сливен сме задружен, забавен и
+      всеотдаен отбор. С нас ще обиколите цяла България и най-красивите и
+      кътчета. Ориентирането ще развие Вашата мисловна дейност и физическата Ви
+      активност. Ще комбинирате приятното с полезното и ще намерите нови
+      приятели. Ако проявявате интерес към този спорт, нашият отбор е отворен за
+      Вас!
+    </p>
+  </div>
+  <div class="home-images">
+    <img
+      class="home-image-1"
+      src="./assets/js/images/homeView/image-1.png"
+      alt=""
+    />
+    <img
+      class="home-image-2"
+      src="./assets/js/images/homeView/image-2.png"
+      alt=""
+    />
+  </div>
+</div>`;
+
+export default { homeView: homeView };
