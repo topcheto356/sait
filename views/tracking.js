05098ee5f7b3561850109095ab796fe6f9689f8b
Kamenow <iavorkamenov777@gmail.com>
2023-11-07 22:55:37 +0200

    tracking page

--- views/tracking.js
+++ views/tracking.js
@@ -1,3 +1,42 @@
-const trackingView = `<p>sex</p>`;
+const trackingView = `
+  <div class="tracking-container">
+    <div class="tracking-info">
+      <h2 class="tracking-info-header">Спортно ориентиране</h2>
+      <p class="tracking-info-text">
+        Спортното ориентиране е спорт, в който от състезателя се изисква да се
+        ориентира в непозната местност сред природата, като се придвижва с
+        помощта на карта и компас. Целта е да се открият и посетят в определен
+        ред контролните точки, отбелязани по предварително очертан маршрут на
+        картата, като се избира възможно най-бързият път, наречен вариянт.
+      </p>
+      <div class="tracking-info-images">
+        <div class="tracking-image-container tracking-info-image-1">
+          <img
+            src="./assets/js/images/trackingView/tracking-info-image-1.png"
+            alt=""
+          />
+          <p>test</p>
+        </div>
+        <div class="tracking-image-container tracking-info-image-2">
+          <img
+            src="./assets/js/images/trackingView/tracking-info-image-2.png"
+            alt=""
+          />
+          <p>test</p>
+        </div>
+      </div>
+      <div class="tracking-info-bike">
+        <h2 class="tracking-info-header"></h2>
+        <p class="tracking-info-text"></p>
+        <div class="tracking-info-images"></div>
+      </div>
+      <div class="tracking-info-sky">
+        <h2 class="tracking-info-header"></h2>
+        <p class="tracking-info-text"></p>
+        <div class="tracking-info-sky-images"></div>
+      </div>
+    </div>
+  </div>
+`;
 
 export default { trackingView: trackingView };
