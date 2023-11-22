e8e2b3bdb99d2eb23feace215bce552dc7f010f7
Kamenow <iavorkamenov777@gmail.com>
2023-11-22 16:27:04 +0200

    club view

--- views/club.js
+++ views/club.js
@@ -1,3 +1,76 @@
-const clubView = `<p>sex na ex</p>`;
+const clubView = `<div class="coaches-container">
+  <div class="coaches-info">
+    <div class="coaches-text-container">
+      <h2 class="coaches-info-header">Треньори</h2>
+      <p class="coaches-info-text">
+        Спортен клуб “Туида” Сливен е основан през 2021 година от Кристина
+        Василева, която е и настоящият треньор с повече от 23 години опит в
+        спорта и множество отличия.
+      </p>
+      <p class="coaches-info-text">
+        Радостин Димитров е председател на клуба с повече от 50 години опит в
+        спорта.
+      </p>
+      <p class="coaches-info-text">
+        Треньор по колоориентиране и ски ориентиране е Ивайло Величков с 11
+        години опит като състезател, 6 години като треньор и е селекционер на
+        Националния отбор по колоориентиране от 3 години. Отличен е с множество
+        награди.
+      </p>
+    </div>
+    <div class="coaches-info-images">
+      <div class="coaches-image-container">
+        <img src="./assets/js/images/coachesView/image-1.png" alt="" />
+        <p>test</p>
+      </div>
+      <div class="coaches-image-container coaches-info-image">
+        <img src="./assets/js/images/coachesView/image-2.png" alt="" />
+        <p>test</p>
+      </div>
+      <div class="coaches-image-container">
+        <img src="./assets/js/images/coachesView/image-3.png" alt="" />
+        <p>test</p>
+      </div>
+    </div>
+  </div>
+  <div class="coaches-info">
+    <div class="coaches-text-container">
+      <h2 class="coaches-info-header">Треньори</h2>
+      <p class="coaches-info-text">
+        Клубът е с повече от 40 състезатели във възрастови групи от 10 до 60
+        години. Едни от най-ярките ни представители са:
+      </p>
+      <p class="coaches-info-text">
+        Жоана Стоянова във възрастова група Ж12 с над 15 медала за по-малко от
+        година.
+      </p>
+      <p class="coaches-info-text">
+        Стефан Куртев във възрастова група М21Е (елит) с над 60 медала.
+      </p>
+      <p class="coaches-info-text">
+        Мирослав Куртев във възрастова група М21Е с над 70 медала и бивш
+        състезател в Националния отбор.
+      </p>
+      <p class="coaches-info-text">
+        Клубът има над 10 купи за 3 години. Също така постигнахме голям успех
+        със състезанието ни “Купа Туида”.
+      </p>
+    </div>
+    <div class="coaches-info-images">
+      <div class="coaches-image-container">
+        <img src="./assets/js/images/coachesView/image-4.png" alt="" />
+        <p>test</p>
+      </div>
+      <div class="coaches-image-container competitors-info-image">
+        <img src="./assets/js/images/coachesView/image-5.png" alt="" />
+        <p>test</p>
+      </div>
+      <div class="coaches-image-container">
+        <img src="./assets/js/images/coachesView/image-5.png" alt="" />
+        <p>test</p>
+      </div>
+    </div>
+  </div>
+</div>`;
 
 export default { clubView: clubView };
