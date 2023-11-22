22c8cc9bee93b63e38bde4215c2ab9ae3d14a42f
Kamenow <iavorkamenov777@gmail.com>
2023-11-22 17:26:02 +0200

    training view

--- views/training.js
+++ views/training.js
@@ -1,3 +1,22 @@
-const trainingView = `<p>training</p>`;
+const trainingView = `<div class="training-container">
+  <div class="training-info">
+    <h2 class="tracking-info-header">Спортно ориентиране</h2>
+    <p class="tracking-info-text">
+      Тренировките по време на учебната година се провеждат всеки вторник и
+      четвъртък като почти винаги са физически, а през уикенда се провежда поне
+      една тренировка, която е техническа (с карта и компас). През лятото
+      програмата е променлива.
+    </p>
+    <div class="training-images-container">
+      <div class="training-images-left-container">
+        <img src="./assets/js/images/trainingView/training-1.webp" alt="" />
+        <img src="./assets/js/images/trainingView/training-2.webp" alt="" />
+      </div>
+      <div class="training-images-right-container">
+        <img src="./assets/js/images/trainingView/training-3.webp" alt="" />
+      </div>
+    </div>
+  </div>
+</div>`;
 
 export default { trainingView: trainingView };
